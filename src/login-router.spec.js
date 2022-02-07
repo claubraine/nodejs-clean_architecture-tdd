2af5d595c9ec10d14fb481ed863e5e93afa8f5ec
claudinei.braine <claudinei.braine@bioma.ind.br>
2022-02-07 16:17:15 -0300

    hould return 400 if no password is provide

--- src/login-router.spec.js
+++ src/login-router.spec.js
@@ -1,22 +1,32 @@
 class LoginRouter {
-    router (httpRequest) {
-        if (!httpRequest.body.mail){
-            return {
-                statusCode: 400
-            }
-        }
+  router (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400
+      }
     }
+  }
 }
 
-describe('Login Router', ()=> {
-    test('Should return 400 if no email is provide', () => {
-        const sut = new LoginRouter()
-        const httoRequest = {
-            body: {
-                password: 'any_password'
-            }
-        }
-        const httpResponse = sut.router(httoRequest)
-        expect(httpResponse.statusCode).toBe(400)
-    })
+describe('Login Router', () => {
+  test('Should return 400 if no email is provide', () => {
+    const sut = new LoginRouter()
+    const httoRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.router(httoRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+  test('Should return 400 if no password is provide', () => {
+    const sut = new LoginRouter()
+    const httoRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.router(httoRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
